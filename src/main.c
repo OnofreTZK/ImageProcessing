0353e12142b0737b687448e1b787953f9ced6cf2
Tiago Onofre <taraujonofre@gmail.com>
2019-05-27 19:52:26 -0300

    filename

--- src/main.c
+++ src/main.c
@@ -1,118 +1,96 @@
 #include "util.h"
 
+const char *getFilename(char *buffer, size_t bf_size) {
+    char *ch = fgets(buffer, bf_size, stdin);
+
+    for(int i = 0; i < bf_size; i++) {
+        if( *(buffer+i) != '\0' && *(ch+i) == '\n') {
+            *(buffer+i) = '\0'; // substitute with '\0'
+        }
+    }
+    return buffer;
+}
 
 int main(int argc, char const *argv[])
 {
-  
-  char cmd[10]; //comando do usuario.
-  int control = 1; //controle do menu.
-  
-  char fname[FILENAME_MAX]; 
 
-  Imagem entrada; //variavel que irá armazenar a imagem.
+    char cmd[10]; //comando do usuario.
+    int control = 1; //controle do menu.
 
 
-  printf("#### PHOTOSHOP20 ####\n\n");
-  
+    Imagem entrada; //variavel que irá armazenar a imagem.
 
 
-  do{
-    
-    printf("\n");
-    printf("qual operação deseja fazer na imagem?\n");
-    printf("$'cin'\t->\tConverte para escala cinza\n");
-    printf("$'thr'\t->\tBinarização da imagem usando thresholding\n");
-    printf("$'blu'\t->\tExecuta blurring\n");
-    printf("$'sha'\t->\tExecuta sharpening\n");
-    printf("$'rot'\t->\tRotaciona a imagem\n");
-    printf("$'amp'\t->\tAmpliar a imagem\n");
-    printf("$'red'\t->\tReduzir a imagem\n");
-    printf("$'exi' para sair\n");
-    printf("\n");
+    printf("#### PHOTOSHOP20 ####\n\n");
 
-    scanf("%s", cmd); 
-    getchar();
 
-    
 
-    if(strcmp(cmd, cin) == 0){
-      LerImagem(argv[1], &entrada);
-      ImagemCinza(&entrada);
-      
-      printf("\nInsira o nome do arquivo acrescentando '.ppm':\n\tEx: 'nome_arquivo.ppm'\n\n");
-      
-      char *ch = fgets(fname, FILENAME_MAX, stdin);
+    do{
+        printf("\n");
+        printf("qual operação deseja fazer na imagem?\n");
+        printf("$'cin'\t->\tConverte para escala cinza\n");
+        printf("$'thr'\t->\tBinarização da imagem usando thresholding\n");
+        printf("$'blu'\t->\tExecuta blurring\n");
+        printf("$'sha'\t->\tExecuta sharpening\n");
+        printf("$'rot'\t->\tRotaciona a imagem\n");
+        printf("$'amp'\t->\tAmpliar a imagem\n");
+        printf("$'red'\t->\tReduzir a imagem\n");
+        printf("$'exi' para sair\n");
+        printf("\n");
 
-          if(ch){
-   
-            while (*ch != '\0' && *ch != '\n') {
-              ch++;
-            }
-  
-          }
-          else if (*ch == '\n'){                  
-            *ch = '\0';
-          }
-      
-      
+        scanf("%s", cmd); 
+        getchar();
 
-      CriarImagem(fname, &entrada);
-    }
-    else if(strcmp(cmd, thr) == 0){
-     	LerImagem(argv[1], &entrada);
-      ImagemCinza(&entrada);
-      Segmentation(&entrada);
-      
-      printf("\nInsira o nome do arquivo acrescentando '.ppm':\n\tEx: 'nome_arquivo.ppm'\n\n");
-      
-      char *ch = fgets(fname, FILENAME_MAX, stdin);
+        char fname[FILENAME_MAX]; 
+        if(strcmp(cmd, cin) == 0){
+            LerImagem(argv[1], &entrada);
+            ImagemCinza(&entrada);
 
-          if(ch){
-   
-            while (*ch != '\0' && *ch != '\n') {
-              ch++;
-            }
-  
-          }
-          else if (*ch == '\n'){                  
-            *ch = '\0';
-          }
-      
-      
+            printf("\nInsira o nome do arquivo acrescentando '.ppm':\n\tEx: 'nome_arquivo.ppm'\n\n");
 
-      CriarImagem(fname, &entrada);
-    }
-    else if(strcmp(cmd, blu) == 0){
-      LerImagem(argv[1], &entrada);
-    }
-    else if(strcmp(cmd, sha) == 0){
-      LerImagem(argv[1], &entrada);
-    }
-    else if(strcmp(cmd, rot) == 0){
-    	LerImagem(argv[1], &entrada);
-    }
-    else if(strcmp(cmd, amp) == 0){
-    	LerImagem(argv[1], &entrada);
-    }
-    else if(strcmp(cmd, red) == 0){
-    	LerImagem(argv[1], &entrada);
-    }
-    else if(strcmp(cmd, exi) == 0){
-        printf("~~~~~~~~~~~~~~~~~~~~~~~\n");
-    	printf("\nPrograma Encerrado\n\n");
-        printf("~~~~~~~~~~~~~~~~~~~~~~~\n");
-    	control = 0;
-    }
-    else{
-    	printf("\n\nComando inválido, tente novamente!\n\n");
-    }
+            // modularizando as coisas né
+            CriarImagem(getFilename(fname, FILENAME_MAX), &entrada);
+        }
+        else if(strcmp(cmd, thr) == 0){
+            LerImagem(argv[1], &entrada);
+            ImagemCinza(&entrada);
+            Segmentation(&entrada);
 
-  }while(control == 1);
+            printf("\nInsira o nome do arquivo acrescentando '.ppm':\n\tEx: 'nome_arquivo.ppm'\n\n");
 
-	
+            CriarImagem(getFilename(fname, FILENAME_MAX), &entrada);
+        }
+        else if(strcmp(cmd, blu) == 0){
+            LerImagem(argv[1], &entrada);
+        }
+        else if(strcmp(cmd, sha) == 0){
+            LerImagem(argv[1], &entrada);
+        }
+        else if(strcmp(cmd, rot) == 0){
+            LerImagem(argv[1], &entrada);
+        }
+        else if(strcmp(cmd, amp) == 0){
+            LerImagem(argv[1], &entrada);
+        }
+        else if(strcmp(cmd, red) == 0){
+            LerImagem(argv[1], &entrada);
+        }
+        else if(strcmp(cmd, exi) == 0){
+            printf("~~~~~~~~~~~~~~~~~~~~~~~\n");
+            printf("\nPrograma Encerrado\n\n");
+            printf("~~~~~~~~~~~~~~~~~~~~~~~\n");
+            control = 0;
+        }
+        else{
+            printf("\n\nComando inválido, tente novamente!\n\n");
+        }
+
+    }while(control == 1);
 
 
 
 
-	return 0;   
+
+
+    return 0;   
 }
