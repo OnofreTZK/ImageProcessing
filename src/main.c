6f9b9dd555cd126a6a20aed8bac68b7217e17ff8
Tiago Onofre <taraujonofre@gmail.com>
2019-05-28 12:03:19 -0300

    pequenas correcoes

--- src/main.c
+++ src/main.c
@@ -1,15 +1,21 @@
 #include "util.h"
 
+
+//Função para o usuário nomear o arquivo para cada operação.
 const char *getFilename(char *buffer, size_t bf_size) {
     char *ch = fgets(buffer, bf_size, stdin);
 
     for(int i = 0; i < bf_size; i++) {
         if( *(buffer+i) != '\0' && *(ch+i) == '\n') {
-            *(buffer+i) = '\0'; // substitute with '\0'
+            *(buffer+i) = '\0'; //Substituição do '\n' pelo '\0'
         }
     }
     return buffer;
 }
+
+
+
+
 
 int main(int argc, char const *argv[])
 {
@@ -41,14 +47,16 @@ int main(int argc, char const *argv[])
         scanf("%s", cmd); 
         getchar();
 
-        char fname[FILENAME_MAX]; 
+        
+        char fname[FILENAME_MAX];//Nome do arquivo que sera processado na func getFilename(). 
+        
+
         if(strcmp(cmd, cin) == 0){
             LerImagem(argv[1], &entrada);
             ImagemCinza(&entrada);
 
             printf("\nInsira o nome do arquivo acrescentando '.ppm':\n\tEx: 'nome_arquivo.ppm'\n\n");
 
-            // modularizando as coisas né
             CriarImagem(getFilename(fname, FILENAME_MAX), &entrada);
         }
         else if(strcmp(cmd, thr) == 0){
